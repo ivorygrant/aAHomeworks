a86eeaa6ca1fa555b80418b8a58d8b9a332a5c44
ivorygrant <haroonahmad06@gmail.com>
2018-01-24 23:00:13 -0500

    Homework W2D4
    Homework W2D4

---
+++ HW W2D4/homework.rb
@@ -0,0 +1,95 @@
+#sluggish Octopus
+
+def quadratic_biggest_fish(fishes)
+  #run two loops
+  longest_fish = ""
+  fishes.each_with_index do |fish1, index1|
+    fishes.each_with_index do |fish2, index2|
+      if fish2.length > fish1.length
+        longest_fish = fish2
+      end
+    end
+  end
+  longest_fish
+end
+
+# n log n search
+class Array
+
+  def merge_sort(&prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+
+    return self if count <= 1
+
+    midpoint = count / 2
+    sorted_left = self.take(midpoint).merge_sort(&prc)
+    sorted_right = self.drop(midpoint).merge_sort(&prc)
+
+    Array.merge(sorted_left, sorted_right, &prc)
+  end
+
+  private
+  def self.merge(left, right, &prc)
+    merged = []
+
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1
+        merged << left.shift
+      when 0
+        merged << left.shift
+      when 1
+        merged << right.shift
+      end
+    end
+
+    merged.concat(left)
+    merged.concat(right)
+
+    merged
+  end
+end
+
+def nlogn_biggest_fish(fishes)
+  # sort the array longest to shortest
+  prc = Proc.new { |x, y| y.length <=> x.length }
+  #return the first element
+  fishes.merge_sort(&prc)[0]
+end
+
+def n_biggest_fish(arr)
+  biggest_fish = ""
+
+  arr.each_with_index do |fish, i|
+    if i != arr.length - 1 && arr[i].length > biggest_fish.length
+      biggest_fish = arr[i]
+    end
+  end
+  biggest_fish
+end
+
+# linear octopus dance
+# tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(direction, tiles_array)
+  tiles_array.each_with_index do |tile, index|
+    return index if tile == direction
+  end
+end
+
+# constant octopus dance
+#use a hash for constant lookup
+tiles_hash = {
+    "up" => 0,
+    "right-up" => 1,
+    "right"=> 2,
+    "right-down" => 3,
+    "down" => 4,
+    "left-down" => 5,
+    "left" => 6,
+    "left-up" => 7
+}
+
+def fast_dance(direction, tiles_hash)
+  tiles_hash[direction]
+end
